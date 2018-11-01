05be981d1aff03988fa585967da94623636e70c6
Ludvik Kemp Gudmundsson <ludvikkemp@gmail.com>
2018-11-01 19:41:51 +0000

    scraping teams and players worksgit status finally

--- football/database/db.js
+++ football/database/db.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
-const leagueShema = require('../schemas/leagueSchema');
-const teamShema = require('../schemas/teamSchema');
+const leagueSchema = require('../schemas/leagueSchema');
+const teamSchema = require('../schemas/teamSchema');
+const playerSchema = require('../schemas/playerSchema');
 
 const connection = mongoose.createConnection(
-  'mongodb://ludvikkemp:xxxxxxxx@ds147833-a0.mlab.com:47833,ds147833-a1.mlab.com:47833/fooballdb?replicaSet=rs-ds147833',
+  'mongodb://ludvikkemp:lulli123@ds147833-a0.mlab.com:47833,ds147833-a1.mlab.com:47833/fooballdb?replicaSet=rs-ds147833',
   {
     useNewUrlParser: true
   }
 );
 
 module.exports = {
-  League: connection.model('League', leagueShema),
-  Team: connection.model('Team', teamShema),
+  League: connection.model('League', leagueSchema),
+  Team: connection.model('Team', teamSchema),
+  Player: connection.model('Player', playerSchema),
   connection
 };
--- football/database/scraper.js
+++ football/database/scraper.js
@@ -1,75 +1,158 @@
 const cheerio = require('cheerio');
 const request = require('request');
-const { League, Team, connection } = require('./db');
+const { League, Team, Player, connection } = require('./db');
 const scrapeInfo = require('./scrape.config');
 
 // Global Variables
 const europeURL = 'https://www.transfermarkt.com/wettbewerbe/europa';
 const mainURL = 'https://www.transfermarkt.com';
+let leagues = [];
+let teams = [];
+let players = [];
 
-class Scraper extends cheerio {
-  constructor() {
-    super();
-    this.properties = {
-      leagues: [],
-      teams: [],
-      players: [],
-      leaguesScraped: false,
-      teamsScraped: false,
-      playersScraped: false
-    };
-  }
+const scrapeLeagues = () => {
+  console.log('---------- STARTING SCRAPING LEAGUES ----------\n');
+  request(
+    {
+      url: europeURL,
+      form: scrapeInfo.form(),
+      headers: scrapeInfo.headers()
+    },
+    (err, res, body) => {
+      if (err) {
+        throw new Error(err);
+      }
+      const $ = cheerio.load(body);
 
-  scrapeLeagues() {
+      // Scraping League Names
+      const leagueNames = $('.responsive-table .items tbody .hauptlink tr a')
+        .toArray()
+        .filter((elem, i) => i % 2 !== 0);
+
+      // Scraping League Nations
+      const leagueNations = $(
+        '.responsive-table .items tbody .zentriert img'
+      ).toArray();
+
+      // Saving each leagueSchema
+      for (let i = 0; i < leagueNames.length; i++) {
+        // New League Schema Created
+        const leagueInstance = new League({
+          transfermarktId: leagueNames[i].attribs.href.split('/', 7)[4],
+          title: leagueNames[i].attribs.title,
+          nation: leagueNations[i].attribs.title,
+          href: leagueNames[i].attribs.href
+        });
+        leagues.push(leagueInstance);
+        // League Schema Saved into Database
+        leagueInstance.save(err => {
+          if (err) {
+            throw new Error(err);
+          }
+        });
+      }
+      console.log('Leagues Successfully Scraped and Saved into Database!\n\n');
+      setTimeout(() => {
+        scrapeTeams();
+      }, 1000);
+    }
+  );
+};
+
+const scrapeTeams = () => {
+  console.log('---------- STARING SCRAPING TEAMS ----------\n');
+  for (let i = 0; i < leagues.length; i++) {
     request(
       {
-        url: europeURL,
+        url: mainURL + leagues[i].href,
         form: scrapeInfo.form(),
         headers: scrapeInfo.headers()
       },
       (err, res, body) => {
         const $ = cheerio.load(body);
-
-        // Scraping League Names
-        const leagueNames = $('.responsive-table .items tbody .hauptlink tr a')
-          .toArray()
-          .filter((elem, i) => i % 2 !== 0);
-
-        // Scraping League Nations
-        const leagueNations = $(
-          '.responsive-table .items tbody .zentriert img'
+        const data = $(
+          '.responsive-table .items tbody .zentriert .vereinprofil_tooltip'
         ).toArray();
+        const teamNames = $(
+          '.responsive-table .items tbody .hauptlink .vereinprofil_tooltip'
+        )
+          .map(function(i, elem) {
+            return $(this).text();
+          })
+          .get()
+          .join(', ')
+          .split(', ')
+          .filter((elem, i) => i % 2 === 0);
 
-        // Saving each leagueSchema
-        for (let i = 0; i < leagueNames.length; i++) {
-          // New League Schema Created
-          const leagueInstance = new League({
-            transfermarktId: leagueNames[i].attribs.href.split('/', 7)[4],
-            title: leagueNames[i].attribs.title,
-            nation: leagueNations[i].attribs.title,
-            href: leagueNames[i].attribs.href
+        for (let j = 0; j < data.length; j++) {
+          console.log('...Adding ' + teamNames[j]);
+          const teamInstance = new Team({
+            transfermarktId: data[j].attribs.href.split('/', 7)[4],
+            name: teamNames[j],
+            href: data[j].attribs.href,
+            league: {
+              transfermarktId: leagues[i].transfermarktId,
+              title: leagues[i].title,
+              nation: leagues[i].nation
+            }
           });
-
-          // League Schema Saved into Database
-          leagueInstance.save(err => {
+          teams.push(teamInstance);
+          teamInstance.save(err => {
             if (err) {
               throw new Error(err);
             }
           });
         }
-        console.log('Successfully saved leagues to database!');
-        this.properties.leaguesScraped = true;
       }
     );
   }
+  setTimeout(() => {
+    console.log('\nTeams Successfully Scraped and Saved into Database!\n\n');
+    scrapePlayers();
+  }, 10000);
+};
 
-  scrapeTeams() {
-    // TODO
+const scrapePlayers = () => {
+  console.log('---------- STARING SCRAPING PLAYERS ----------\n');
+
+  for (let i = 0; i < teams.length; i++) {
+    request(
+      {
+        url: mainURL + teams[i].href,
+        form: scrapeInfo.form(),
+        headers: scrapeInfo.headers()
+      },
+      (err, res, body) => {
+        const $ = cheerio.load(body);
+        const data = $('span.hide-for-small a.spielprofil_tooltip').toArray();
+
+        for (let j = 0; j < data.length; j++) {
+          console.log('...Adding ' + data[j].attribs.title);
+          const playerInstance = new Player({
+            transfermarktId: data[j].attribs.id,
+            name: data[j].attribs.title,
+            url: data[j].attribs.href,
+            team: {
+              transfermarktId: teams[i].transfermarktId,
+              name: teams[i].name
+            }
+          });
+          players.push(playerInstance);
+          playerInstance.save(err => {
+            if (err) {
+              throw new Error(err);
+            }
+          });
+        }
+      }
+    );
   }
+  setTimeout(() => {
+    console.log('\nPlayers Successfully Scraped and Saved into Database\n\n');
+    console.log(
+      'Mongo Database for football players and teams in 25 leagues in Europe'
+    );
+  }, 50000);
+};
 
-  scrapePlayers() {
-    // TODO
-  }
-}
-
-module.exports = Scraper;
+scrapeLeagues();
